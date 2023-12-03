ace4ca58292492542d3aa22ea62d732cc9d80196
kittywizard <volcana@gmail.com>
2023-12-03 14:00:30 -0800

    adding auth

---
+++ api/db/migrations/20231203213815_auth_setup/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[webAuthnChallenge]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `hashedPassword` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `salt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "hashedPassword" TEXT NOT NULL,
+ADD COLUMN     "resetToken" TEXT,
+ADD COLUMN     "resetTokenExpiresAt" TIMESTAMP(3),
+ADD COLUMN     "salt" TEXT NOT NULL,
+ADD COLUMN     "webAuthnChallenge" TEXT;
+
+-- CreateTable
+CREATE TABLE "UserCredential" (
+    "id" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "publicKey" BYTEA NOT NULL,
+    "transports" TEXT,
+    "counter" BIGINT NOT NULL,
+
+    CONSTRAINT "UserCredential_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_webAuthnChallenge_key" ON "User"("webAuthnChallenge");
+
+-- AddForeignKey
+ALTER TABLE "UserCredential" ADD CONSTRAINT "UserCredential_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
